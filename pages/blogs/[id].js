e95de4522bb9562fab239763a38e006fa90aef85
unknown <akhileshgautam19@gmail.com>
2023-05-21 22:08:28 +0530

    bug fixes, add ISR

--- pages/blogs/[id].js
+++ pages/blogs/[id].js
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 
 export const getStaticPaths = async() =>{
 
@@ -26,17 +24,19 @@ export const getStaticProps = async(context) =>{
     const data = await res.json();
 
     return {
-        props: { blog: data }
-    }
+        props: { blog: data },
+        revalidate: 10,
+    };
 }
 
 export default function Details ({ blog }) {
+    
     return(
         <div className="my-8">
             <h1 className="text-6xl font-bold dark:text-white">{ blog.title }</h1>
             <p className="text-zinc-500  my-3">Author - { blog.author }</p>
-            <Image className="my-2 rounded-sm" src={ blog.image } width={720} height={800} />
-            <p className="my-3 dark:text-zinc-300" dangerouslySetInnerHTML={{__html: blog.content}}></p>
+            <img className="my-2 rounded-sm" alt="blog-img" src={ blog.image } width={720} height={800} />
+            <div className="my-3 dark:text-zinc-300" dangerouslySetInnerHTML={{__html: blog.content}}></div>
         </div>
     )
 }
--- pages/blogs/index.js
+++ pages/blogs/index.js
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import Image from "next/image";
 
 export const getStaticProps = async () => {
 
@@ -7,8 +6,9 @@ export const getStaticProps = async () => {
     const data = await res.json();
 
     return{
-        props: { blogs: data }
-    }
+        props: { blogs: data },
+        revalidate: 10,
+    };
 }
 
 export default function Blogs({ blogs }) {
@@ -19,7 +19,7 @@ export default function Blogs({ blogs }) {
                 <Link href={`/blogs/${ blog.id }`} key={ blog.id } >
                     <div className="px-6 py-5 shadow-md my-2 rounded-sm border-s-8 border-s-white dark:border-s-slate-900 dark:hover:border-s-blue-500 hover:border-s-blue-500">
                         <div className="md:flex">
-                            <Image className="rounded-md md:mx-3 my-2 mx-0 md:h-28 md:w-40 w-full" src={ blog.image } height={800} width={800}/>
+                            <img className="rounded-md md:mx-3 my-2 mx-0 md:h-28 md:w-40 w-full" alt="thumbnail" src={ blog.image } height={800} width={800}/>
                             <div className="">
                                 <h3 className="text-xl font-medium text-zinc-700 dark:text-slate-400">{ blog.title }</h3>
                                 <p className="text-sm text-gray-500 mt-4">{ blog.author }</p>
