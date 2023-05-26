ec4c3eb604a1f0d7e328db1457506814156aeb4e
unknown <akhileshgautam19@gmail.com>
2023-05-26 22:34:56 +0530

    bug fixes

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '@/styles/globals.css'
 import Layout from '@/components/Layout'
+import 'react-quill/dist/quill.snow.css'
 
 export default function App({ Component, pageProps }) {
   return (
