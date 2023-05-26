85b59c77593ab5d5085761c3062c95cc9bfa10de
unknown <akhileshgautam19@gmail.com>
2023-05-26 18:24:09 +0530

    remove dark theme from blog page

--- pages/blogs/[id].js
+++ pages/blogs/[id].js
@@ -16,7 +16,7 @@ export const getStaticPaths = async() =>{
 
     return{
         paths,
-        fallback: false
+        fallback: true
     }
 
 }
@@ -41,10 +41,10 @@ export default function Details ({ blog }) {
     return(
         <div className="min-h-screen" style={{backgroundColor: `${blog.backgroundColor}`}}>
             <div className="w-full mx-auto py-6 sm:w-2/5">
-                <h1 className="text-6xl font-bold dark:text-white" style={{color: `${blog.titleColor}`}}>{ blog.title }</h1>
+                <h1 className="text-6xl font-bold " style={{color: `${blog.titleColor}`}}>{ blog.title }</h1>
                 <p className="text-zinc-500  my-3">Author - { blog.author }</p>
                 <img className="my-2 rounded-sm" alt="blog-img" src={ blog.image } width={720} height={800} />
-                <div className="my-3 dark:text-zinc-300" dangerouslySetInnerHTML={{__html: blog.content}}></div>
+                <div className="my-3 ql-editor" dangerouslySetInnerHTML={{__html: blog.content}}></div>
                 <button className="rounded-md p-2 my-4" style={{backgroundColor: `${blog.titleColor}`, color: `${blog.backgroundColor}`}} onClick={() => router.push('/blogs')}>Go back to home</button>
             </div>
         </div>
